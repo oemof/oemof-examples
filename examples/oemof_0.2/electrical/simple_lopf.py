61701b5bf87418196f3ffa4bc50a14eae74fff9d
simnh <simon.hilpert@uni-flensburg.de>
2017-12-06 07:45:43 +0100

    Adapt example

--- examples/oemof_0.2/electrical/simple_lopf.py
+++ examples/oemof_0.2/electrical/simple_lopf.py
@@ -8,7 +8,6 @@ from oemof.solph import (EnergySystem, Model, Bus, Flow, Source, Sink, custom)
 from oemof.outputlib import processing, views
 from oemof.outputlib.graph_tools import graph
 
-
 datetimeindex = pd.date_range('1/1/2012', periods=2, freq='H')
 
 es = EnergySystem(timeindex=datetimeindex)
@@ -23,20 +22,20 @@ b_el2 = custom.ElectricalBus(label="b_el2", v_min=-1, v_max=1)
 
 es.add(b_el0, b_el1, b_el2)
 
-es.add(custom.ElectricalLine(label="line1",
+es.add(custom.ElectricalLine(label="line0",
                              inputs={b_el0: Flow()},
                              outputs={b_el1: Flow(nominal_value=60,
-                                                  min=-1, max=1)},
+                                                  min=-1000, max=1000)},
                              reactance=0.0001))
-es.add(custom.ElectricalLine(label="line2",
+es.add(custom.ElectricalLine(label="line1",
                              inputs={b_el1: Flow()},
                              outputs={b_el2: Flow(nominal_value=60,
-                                                  min=-1, max=1)},
+                                                  min=-1000, max=1000)},
                              reactance=0.0001))
-es.add(custom.ElectricalLine(label="line3",
+es.add(custom.ElectricalLine(label="line2",
                              inputs={b_el2: Flow()},
                              outputs={b_el0: Flow(nominal_value=60,
-                                                  min=-1, max=1)},
+                                                  min=-1000, max=1000)},
                              reactance=0.0001))
 
 es.add(Source(label="gen_0", outputs={b_el0: Flow(nominal_value=100,
