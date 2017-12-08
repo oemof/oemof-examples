c94f4f2c31256c4bbfe944d45ee7eb2f7ced423b
simnh <simon.hilpert@uni-flensburg.de>
2017-12-08 17:37:10 +0100

    Fix lopf example

--- examples/oemof_0.2/electrical/simple_lopf.py
+++ examples/oemof_0.2/electrical/simple_lopf.py
@@ -4,7 +4,8 @@
 import pandas as pd
 
 # solph imports
-from oemof.solph import (EnergySystem, Model, Bus, Flow, Source, Sink, custom)
+from oemof.solph import (EnergySystem, Model, Flow, Source, Sink, custom,
+                         Investment)
 from oemof.outputlib import processing, views
 from oemof.outputlib.graph_tools import graph
 
@@ -12,8 +13,6 @@ datetimeindex = pd.date_range('1/1/2012', periods=2, freq='H')
 
 es = EnergySystem(timeindex=datetimeindex)
 
-
-# electricity and heat
 b_el0 = custom.ElectricalBus(label="b_0", v_min=-1, v_max=1)
 
 b_el1 = custom.ElectricalBus(label="b_1", v_min=-1, v_max=1)
@@ -23,20 +22,19 @@ b_el2 = custom.ElectricalBus(label="b_2", v_min=-1, v_max=1)
 es.add(b_el0, b_el1, b_el2)
 
 es.add(custom.ElectricalLine(label="line0",
-                             inputs={b_el0: Flow(nominal_value=60,
-                                                  min=-1, max=1)},
-                             outputs={b_el1: Flow(nominal_value=60,
+                             inputs={b_el0: Flow()},
+                             outputs={b_el1: Flow(investment=Investment(),
                                                   min=-1, max=1)},
                              reactance=0.0001))
+
 es.add(custom.ElectricalLine(label="line1",
-                             inputs={b_el1: Flow(nominal_value=60,
-                                                  min=-1, max=1)},
+                             inputs={b_el1: Flow()},
                              outputs={b_el2: Flow(nominal_value=60,
                                                   min=-1, max=1)},
                              reactance=0.0001))
+
 es.add(custom.ElectricalLine(label="line2",
-                             inputs={b_el2: Flow(nominal_value=60,
-                                                  min=-1, max=1)},
+                             inputs={b_el2: Flow()},
                              outputs={b_el0: Flow(nominal_value=60,
                                                   min=-1, max=1)},
                              reactance=0.0001))
