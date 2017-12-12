8780e722db24a1007320e275017c9444f2615295
uwe <uwe.krien@rl-institut.de>
2017-12-12 12:48:37 +0100

    clean plotting example to focus on plotting part

--- examples/oemof_0.2/plotting_examples/variable_chp_plot.py
+++ examples/oemof_0.2/plotting_examples/variable_chp_plot.py
@@ -65,74 +65,53 @@ def shape_legend(node, reverse=False, **kwargs):
     return axes
 
 
-# Parameter
-solver = 'cbc'
-smooth_plot = True
-
-# logger.define_logging(text="Starting chp example", screen_level=logging.DEBUG)
 logger.define_logging()
 logging.info('Initialize the energy system')
-
-# create time index for 192 hours in May.
 date_time_index = pd.date_range('5/5/2012', periods=192, freq='H')
 energysystem = solph.EnergySystem(timeindex=date_time_index)
 Node.registry = energysystem
 
-# Read data file with heat and electrical demand (192 hours)
 full_filename = os.path.join(os.path.dirname(__file__), 'variable_chp.csv')
 data = pd.read_csv(full_filename, sep=",")
 
-##########################################################################
-# Create oemof.solph objects
-##########################################################################
-
 logging.info('Create oemof.solph objects')
 
-# create natural gas bus
 bgas = solph.Bus(label="natural_gas")
-
-# create commodity object for gas resource
 solph.Source(label='rgas', outputs={bgas: solph.Flow(variable_costs=50)})
 
-# create two electricity buses and two heat buses
 bel = solph.Bus(label="electricity")
 bel2 = solph.Bus(label="electricity_2")
 bth = solph.Bus(label="heat")
 bth2 = solph.Bus(label="heat_2")
 
-# create excess components for the elec/heat bus to allow overproduction
 solph.Sink(label='excess_bth_2', inputs={bth2: solph.Flow()})
 solph.Sink(label='excess_therm', inputs={bth: solph.Flow()})
 solph.Sink(label='excess_bel_2', inputs={bel2: solph.Flow()})
 solph.Sink(label='excess_elec', inputs={bel: solph.Flow()})
 
-# create simple sink object for electrical demand for each electrical bus
 solph.Sink(label='demand_elec', inputs={bel: solph.Flow(
     actual_value=data['demand_el'], fixed=True, nominal_value=1)})
 solph.Sink(label='demand_el_2', inputs={bel2: solph.Flow(
     actual_value=data['demand_el'], fixed=True, nominal_value=1)})
 
-# create simple sink object for heat demand for each thermal bus
 solph.Sink(label='demand_therm', inputs={bth: solph.Flow(
     actual_value=data['demand_th'], fixed=True, nominal_value=741000)})
 solph.Sink(label='demand_th_2', inputs={bth2: solph.Flow(
     actual_value=data['demand_th'], fixed=True, nominal_value=741000)})
 
-# This is just a dummy transformer with a nominal input of zero
+# This is just a dummy transformer with a nominal input of zero (for the plot)
 solph.Transformer(
     label='fixed_chp_gas',
     inputs={bgas: solph.Flow(nominal_value=0)},
     outputs={bel: solph.Flow(), bth: solph.Flow()},
     conversion_factors={bel: 0.3, bth: 0.5})
 
-# create a fixed transformer to distribute to the heat_2 and elec_2 buses
 solph.Transformer(
     label='fixed_chp_gas_2',
     inputs={bgas: solph.Flow(nominal_value=10e10)},
     outputs={bel2: solph.Flow(), bth2: solph.Flow()},
     conversion_factors={bel2: 0.3, bth2: 0.5})
 
-# create a fixed transformer to distribute to the heat and elec buses
 solph.components.ExtractionTurbineCHP(
     label='variable_chp_gas',
     inputs={bgas: solph.Flow(nominal_value=10e10)},
@@ -141,16 +120,10 @@ solph.components.ExtractionTurbineCHP(
     conversion_factor_full_condensation={bel: 0.5}
     )
 
-##########################################################################
-# Optimise the energy system and plot the results
-##########################################################################
-
 logging.info('Optimise the energy system')
-
 om = solph.Model(energysystem)
-
 logging.info('Solve the optimization problem')
-om.solve(solver=solver, solve_kwargs={'tee': False})
+om.solve(solver='cbc', solve_kwargs={'tee': False})
 
 results = outputlib.processing.results(om)
 
@@ -159,6 +132,7 @@ results = outputlib.processing.results(om)
 ##########################################################################
 
 logging.info('Plot the results')
+smooth_plot = True
 
 cdict = {
     (('variable_chp_gas', 'electricity'), 'flow'): '#42c77a',
