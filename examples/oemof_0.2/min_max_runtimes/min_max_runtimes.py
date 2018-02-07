2eb4cdb543cf4fbb39c46e7cbfa1283dd9d97f36
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2018-02-07 12:25:28 +0100

    Add basic min-max example

---
+++ examples/oemof_0.2/min_max_runtimes/min_max_runtimes.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+General description
+-------------------
+Example that illustrates how to model min and max runtimes.
+
+Installation requirements
+-------------------------
+This example requires the latest version of oemof. Install by:
+
+    pip install oemof
+
+"""
+
+import pandas as pd
+import oemof.solph as solph
+from oemof.network import Node
+from oemof.outputlib import processing, views
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    plt = None
+
+
+# read sequence data
+file_name = 'data'
+data = pd.read_csv(file_name + '.csv', sep=",")
+
+# select periods
+periods = len(data)-1
+
+# create an energy system
+idx = pd.date_range('1/1/2017', periods=periods, freq='H')
+es = solph.EnergySystem(timeindex=idx)
+Node.registry = es
+
+# power bus and components
+bel = solph.Bus(label='bel')
+
+demand_el = solph.Sink(
+    label='demand_el',
+    inputs={bel: solph.Flow(
+        fixed=True, actual_value=data['demand_el'], nominal_value=10)})
+
+dummy_el = solph.Sink(
+    label='dummy_el',
+    inputs={bel: solph.Flow(variable_costs=10)})
+
+pp = solph.Source(
+    label='power_plant',
+    outputs={
+        bel: solph.Flow(
+            nominal_value=10, min=0.5, max=1.0, variable_costs=10,
+            nonconvex=solph.NonConvex(minimum_uptime=5, minimum_downtime=2))})
+
+# create an optimization problem and solve it
+om = solph.Model(es)
+
+# debugging
+#om.write('problem.lp', io_options={'symbolic_solver_labels': True})
+
+# solve model
+om.solve(solver='cbc', solve_kwargs={'tee': True})
+
+# create result object
+results = processing.results(om)
+
+# plot data
+if plt is not None:
+    # plot electrical bus
+    data = views.node(results, 'bel')['sequences']
+    data[[(('bel', 'demand_el'), 'flow'), (('bel', 'dummy_el'), 'flow')]] *= -1
+    columns = [c for c in data.columns
+               if not any(s in c for s in ['status'])]
+    data = data[columns]
+    ax = data.plot(kind='line', drawstyle='steps-post', grid=True, rot=0)
+    ax.set_xlabel('Hour')
+    ax.set_ylabel('P (MW)')
+    plt.show()
