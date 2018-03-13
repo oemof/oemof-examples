9c3c032b1ac2b334e5070898e2a82d2552f03984
simnh <simon.hilpert@uni-flensburg.de>
2018-03-13 17:29:01 +0100

    update facades example

--- examples/oemof_0.2/facades/model.py
+++ examples/oemof_0.2/facades/model.py
@@ -4,7 +4,8 @@
 import os
 import pandas as pd
 from oemof.solph import Bus, EnergySystem, Model
-from oemof.solph.facades import Demand, Generator, Storage, Connector, CHP
+from oemof.solph.facades import (Demand, Generator, Storage, Connection, CHP,
+                                 Conversion)
 from oemof.outputlib import processing, views
 
 es = EnergySystem(timeindex=pd.date_range(start='2018', periods=4, freq='H'))
@@ -13,21 +14,31 @@ b0 = Bus(label='b0')
 
 b1 = Bus(label='b1')
 
-connection  = Connector(label='connector', loss=0.03, capacity=None, capex=1500,
-                        from_bus=b0, to_bus=b1)
+bg = Bus(label='bg')
 
-supply = Generator(label='generator', capacity=1000, opex=10,
+# create components
+conversion = Conversion(label='conversion', from_bus=bg, to_bus=b0, capacity=10,
+                        efficiency=0.3, marginal_cost=100)
+
+connection  = Connection(label='connection', loss=0.03, capacity=None,
+                         investment_cost=1500,
+                         from_bus=b0, to_bus=b1)
+
+supply = Generator(label='generator', capacity=1000, marginal_cost=10,
                    bus=b0)
+
 
 wind = Generator(label='wind', capacity=110, dispatchable=False,
                  profile=[1] * 4, bus=b1)
 
-storage = Storage(label='storage', capacity=None, capex=2000, bus=b0)
+storage = Storage(label='storage', capacity=None,
+                  investment_cost=2000, bus=b0)
 
 demand = Demand(label='demand', amount=200, profile=[1] * 4, bus=b0)
 
+
 # add components to energy system
-es.add(b0, b1, supply, wind, demand, storage, connection)
+es.add(b0, b1, bg, supply, wind, demand, storage, connection, conversion)
 
 # construct model from energy system
 m = Model(es)
