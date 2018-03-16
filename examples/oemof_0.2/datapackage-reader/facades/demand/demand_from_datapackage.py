d8e975159ed3ec322ff21d2a763f48798c350c75
simnh <simon.hilpert@uni-flensburg.de>
2018-03-16 16:00:49 +0100

    Add foreign keys to exmaple

--- examples/oemof_0.2/datapackage-reader/facades/demand/demand_from_datapackage.py
+++ examples/oemof_0.2/datapackage-reader/facades/demand/demand_from_datapackage.py
@@ -4,13 +4,27 @@
 import os
 import os.path
 
+from datapackage import Package
+
 from oemof.solph import Bus, EnergySystem
 from oemof.solph.facades import Demand, Generator
 
+
+path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+             'datapackage',
+             'datapackage.json')
+
+p = Package(path)
+
+if p.valid:
+    try:
+        for r in p.resources:
+            [e for e in r.iter(relations=True)]
+    except:
+        raise ValueError('Your relations / foreign keys are messed up!')
+
 es = EnergySystem.from_datapackage(
-    os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                 'datapackage',
-                 'datapackage.json'),
+    path,
     attributemap={
         Demand: {"demand-profiles": "profile"}},
     typemap={
