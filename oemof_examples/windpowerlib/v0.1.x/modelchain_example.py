7235aa9d8450cb2d3a48669835a6d3042cf7dcff
uvchik <uvchik.git@posteo.eu>
2019-06-24 16:33:11 +0200

    Fix windpowerlib example for old pandas versions

--- oemof_examples/windpowerlib/v0.1.x/modelchain_example.py
+++ oemof_examples/windpowerlib/v0.1.x/modelchain_example.py
@@ -71,9 +71,9 @@ weather = pd.read_csv(filename, index_col=0, header=[0, 1],
 weather.index = pd.to_datetime(weather.index).tz_convert('Europe/Berlin')
 
 # change type of height from str to int by resetting columns
-weather.columns = [
-    weather.axes[1].levels[0][weather.axes[1].codes[0]],
-    weather.axes[1].levels[1][weather.axes[1].codes[1]].astype(int)]
+l0 = [_[0] for _ in weather.columns]
+l1 = [int(_[1]) for _ in weather.columns]
+weather.columns = [l0, l1]
 
 
 ##########################################################################
