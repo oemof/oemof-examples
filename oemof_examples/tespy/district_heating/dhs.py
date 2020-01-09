05c11dfabf7eec0d9c6aa4c1e04beb9e54074744
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-01-09 10:03:55 +0100

    Add parameter for pipe temperature difference

--- oemof_examples/tespy/district_heating/dhs.py
+++ oemof_examples/tespy/district_heating/dhs.py
@@ -148,6 +148,10 @@ h4.comps['return_1'].set_attr(ks=7e-5, L=10, D=0.04, offdesign=['kA'])
 h4.comps['return_2'].set_attr(ks=7e-5, L=10, D=0.04, offdesign=['kA'])
 
 
+# temperature difference factor for pipes:
+
+dT_feed = 100
+dT_return = 200
 # return temperatures of consumers
 
 for sub in [h1, ia, sc, h2, h3, h4]:
@@ -161,28 +165,28 @@ for sub in [h1, ia, sc, h2, h3, h4]:
         for i in range(sub.num_consumer - 1):
 
             # feed
-            dT_feed = ref(sub.conns['spfe_' + str(i)], 1,
-                          -sub.comps['feed_' + str(i)].L.val / 100)
+            dT_feed_ref = ref(sub.conns['spfe_' + str(i)], 1,
+                              -sub.comps['feed_' + str(i)].L.val / dT_feed)
             # return
-            dT_return = ref(sub.conns['mere_' + str(i + 1)], 1,
-                            -sub.comps['return_' + str(i)].L.val / 200)
+            dT_return_ref = ref(sub.conns['mere_' + str(i + 1)], 1,
+                                -sub.comps['return_' + str(i)].L.val / dT_return)
 
-            sub.conns['fesp_' + str(i + 1)].set_attr(T=dT_feed, design=['T'])
-            sub.conns['reme_' + str(i)].set_attr(T=dT_return, design=['T'])
+            sub.conns['fesp_' + str(i + 1)].set_attr(T=dT_feed_ref, design=['T'])
+            sub.conns['reme_' + str(i)].set_attr(T=dT_return_ref, design=['T'])
 
     elif isinstance(sub, lo):
 
         for i in range(sub.num_consumer - 1):
 
             # feed
-            dT_feed = ref(sub.conns['spfe_' + str(i)], 1,
-                          -sub.comps['feed_' + str(i)].L.val / 100)
+            dT_feed_ref = ref(sub.conns['spfe_' + str(i)], 1,
+                              -sub.comps['feed_' + str(i)].L.val / dT_feed)
             # return
-            dT_return = ref(sub.conns['mere_' + str(i + 1)], 1,
-                            -sub.comps['return_' + str(i)].L.val / 200)
+            dT_return_ref = ref(sub.conns['mere_' + str(i + 1)], 1,
+                                -sub.comps['return_' + str(i)].L.val / dT_return)
 
-            sub.conns['fesp_' + str(i + 1)].set_attr(T=dT_feed, design=['T'])
-            sub.conns['reme_' + str(i)].set_attr(T=dT_return, design=['T'])
+            sub.conns['fesp_' + str(i + 1)].set_attr(T=dT_feed_ref, design=['T'])
+            sub.conns['reme_' + str(i)].set_attr(T=dT_return_ref, design=['T'])
 
 
 # %% connections
@@ -191,15 +195,15 @@ for sub in [h1, ia, sc, h2, h3, h4]:
 
 # feed
 so_pif1 = connection(so, 'out1', pif1, 'in1', T=90, p=15, fluid={'water': 1})
-pif1_k1f = connection(pif1, 'out1', k1.comps['splitter'], 'in1', T=ref(so_pif1, 1, -1*pif1.L.val/100), design=['T'])
+pif1_k1f = connection(pif1, 'out1', k1.comps['splitter'], 'in1', T=ref(so_pif1, 1, -pif1.L.val / dT_feed), design=['T'])
 k1f_pif2 = connection(k1.comps['splitter'], 'out1', pif2, 'in1')
-pif2_h1 = connection(pif2, 'out1', h1.comps['splitter_0'], 'in1', T=ref(pif1_k1f, 1, -1*pif2.L.val/100), design=['T'])
+pif2_h1 = connection(pif2, 'out1', h1.comps['splitter_0'], 'in1', T=ref(pif1_k1f, 1, -pif2.L.val / dT_feed), design=['T'])
 
 # back
 h1_pib2 = connection(h1.comps['merge_0'], 'out1', pib2, 'in1')
-pib2_k1 = connection(pib2, 'out1', k1.comps['valve_0'], 'in1', T=ref(h1_pib2, 1, -1*pib2.L.val/200), design=['T'])
+pib2_k1 = connection(pib2, 'out1', k1.comps['valve_0'], 'in1', T=ref(h1_pib2, 1, -pib2.L.val / dT_return), design=['T'])
 k1_pib1 = connection(k1.comps['merge'], 'out1', pib1, 'in1', p=11)
-pib1_si = connection(pib1, 'out1', si, 'in1', T=ref(k1_pib1, 1, -1*pib1.L.val/200), design=['T'])
+pib1_si = connection(pib1, 'out1', si, 'in1', T=ref(k1_pib1, 1, -pib1.L.val / dT_return), design=['T'])
 
 nw.add_conns(so_pif1, pif1_k1f, k1f_pif2, pif2_h1)
 nw.add_conns(h1_pib2, pib2_k1, k1_pib1, pib1_si)
@@ -210,15 +214,15 @@ nw.add_subsys(h1)
 
 # feed
 k1_pif4 = connection(k1.comps['splitter'], 'out2', pif4, 'in1')
-pif4_v = connection(pif4, 'out1', ia.comps['splitter_0'], 'in1', T=ref(k1_pif4, 1, -1*pif4.L.val/100), design=['T'])
+pif4_v = connection(pif4, 'out1', ia.comps['splitter_0'], 'in1', T=ref(k1_pif4, 1, -pif4.L.val / dT_feed), design=['T'])
 v_pif7 = connection(ia.comps['splitter_2'], 'out2', pif7, 'in1')
-pif7_k2 = connection(pif7, 'out1', k2.comps['splitter'], 'in1', T=ref(v_pif7, 1, -1*pif7.L.val/100), design=['T'])
+pif7_k2 = connection(pif7, 'out1', k2.comps['splitter'], 'in1', T=ref(v_pif7, 1, -pif7.L.val / dT_feed), design=['T'])
 
 # back
 k2_pib7 = connection(k2.comps['merge'], 'out1', pib7, 'in1', p=12)
-pib7_v = connection(pib7, 'out1', ia.comps['merge_2'], 'in1', T=ref(k2_pib7, 1, -1*pib7.L.val/200), design=['T'])
+pib7_v = connection(pib7, 'out1', ia.comps['merge_2'], 'in1', T=ref(k2_pib7, 1, -pib7.L.val / dT_return), design=['T'])
 v_pib4 = connection(ia.comps['merge_0'], 'out1', pib4, 'in1')
-pib4_k1 = connection(pib4, 'out1', k1.comps['valve_1'], 'in1', T=ref(v_pib4, 1, -1*pib4.L.val/200), design=['T'])
+pib4_k1 = connection(pib4, 'out1', k1.comps['valve_1'], 'in1', T=ref(v_pib4, 1, -pib4.L.val / dT_return), design=['T'])
 
 nw.add_conns(k1_pif4, pif4_v, v_pif7, pif7_k2)
 nw.add_conns(k2_pib7, pib7_v, v_pib4, pib4_k1)
@@ -228,11 +232,11 @@ nw.add_subsys(ia)
 
 # feed
 k2_pif8 = connection(k2.comps['splitter'], 'out1', pif8, 'in1')
-pif8_sc = connection(pif8, 'out1', sc.comps['splitter_0'], 'in1', T=ref(k2_pif8, 1, -1*pif8.L.val/100), design=['T'])
+pif8_sc = connection(pif8, 'out1', sc.comps['splitter_0'], 'in1', T=ref(k2_pif8, 1, -pif8.L.val / dT_feed), design=['T'])
 
 # back
 sc_pib8 = connection(sc.comps['merge_0'], 'out1', pib8, 'in1')
-pib8_k2 = connection(pib8, 'out1', k2.comps['valve_0'], 'in1', T=ref(sc_pib8, 1, -1*pib8.L.val/200), design=['T'])
+pib8_k2 = connection(pib8, 'out1', k2.comps['valve_0'], 'in1', T=ref(sc_pib8, 1, -pib8.L.val / dT_return), design=['T'])
 
 nw.add_conns(k2_pif8, pif8_sc)
 nw.add_conns(sc_pib8, pib8_k2)
@@ -242,15 +246,15 @@ nw.add_subsys(sc)
 
 # feed
 k2_pif10 = connection(k2.comps['splitter'], 'out2', pif10, 'in1')
-pif10_k3 = connection(pif10, 'out1', k3.comps['splitter'], 'in1', T=ref(k2_pif10, 1, -1*pif10.L.val/100), design=['T'])
+pif10_k3 = connection(pif10, 'out1', k3.comps['splitter'], 'in1', T=ref(k2_pif10, 1, -pif10.L.val / dT_feed), design=['T'])
 k3_pif11 = connection(k3.comps['splitter'], 'out1', pif11, 'in1')
-pif11_h2 = connection(pif11, 'out1', h2.comps['splitter_0'], 'in1', T=ref(k3_pif11, 1, -1*pif11.L.val/100), design=['T'])
+pif11_h2 = connection(pif11, 'out1', h2.comps['splitter_0'], 'in1', T=ref(k3_pif11, 1, -pif11.L.val / dT_feed), design=['T'])
 
 # back
 h2_pib11 = connection(h2.comps['merge_0'], 'out1', pib11, 'in1')
-pib11_k3 = connection(pib11, 'out1', k3.comps['valve_0'], 'in1', T=ref(h2_pib11, 1, -1*pib11.L.val/200), design=['T'])
+pib11_k3 = connection(pib11, 'out1', k3.comps['valve_0'], 'in1', T=ref(h2_pib11, 1, -pib11.L.val / dT_return), design=['T'])
 k3_pib10 = connection(k3.comps['merge'], 'out1', pib10, 'in1', p=12.5)
-pib10_k2 = connection(pib10, 'out1', k2.comps['valve_1'], 'in1', T=ref(k3_pib10, 1, -1*pib10.L.val/200), design=['T'])
+pib10_k2 = connection(pib10, 'out1', k2.comps['valve_1'], 'in1', T=ref(k3_pib10, 1, -pib10.L.val / dT_return), design=['T'])
 #
 nw.add_conns(k2_pif10, pif10_k3, k3_pif11, pif11_h2)
 nw.add_conns(h2_pib11, pib11_k3, k3_pib10, pib10_k2)
@@ -260,15 +264,15 @@ nw.add_subsys(h2)
 
 # feed
 k3_pif16 = connection(k3.comps['splitter'], 'out2', pif16, 'in1')
-pif16_k4 = connection(pif16, 'out1', k4.comps['splitter'], 'in1', T=ref(k3_pif16, 1, -1*pif16.L.val/100), design=['T'])
+pif16_k4 = connection(pif16, 'out1', k4.comps['splitter'], 'in1', T=ref(k3_pif16, 1, -pif16.L.val / dT_feed), design=['T'])
 k4_pif17 = connection(k4.comps['splitter'], 'out1', pif17, 'in1')
-pif17_h3 = connection(pif17, 'out1', h3.comps['splitter_0'], 'in1', T=ref(k4_pif17, 1, -1*pif17.L.val/100), design=['T'])
+pif17_h3 = connection(pif17, 'out1', h3.comps['splitter_0'], 'in1', T=ref(k4_pif17, 1, -pif17.L.val / dT_feed), design=['T'])
 
 # back
 h3_pib17 = connection(h3.comps['merge_0'], 'out1', pib17, 'in1')
-pib17_k4 = connection(pib17, 'out1', k4.comps['valve_0'], 'in1',T=ref(h3_pib17, 1, -1*pib17.L.val/200), design=['T'])
+pib17_k4 = connection(pib17, 'out1', k4.comps['valve_0'], 'in1',T=ref(h3_pib17, 1, -pib17.L.val / dT_return), design=['T'])
 k4_pib16 = connection(k4.comps['merge'], 'out1', pib16, 'in1', p=12.75)
-pib16_k3 = connection(pib16, 'out1', k3.comps['valve_1'], 'in1', T=ref(k4_pib16, 1, -1*pib16.L.val/200), design=['T'])
+pib16_k3 = connection(pib16, 'out1', k3.comps['valve_1'], 'in1', T=ref(k4_pib16, 1, -pib16.L.val / dT_return), design=['T'])
 
 nw.add_conns(k3_pif16, pif16_k4, k4_pif17, pif17_h3)
 nw.add_conns(h3_pib17, pib17_k4, k4_pib16, pib16_k3)
@@ -278,11 +282,11 @@ nw.add_subsys(h3)
 
 # feed
 k4_pif21 = connection(k4.comps['splitter'], 'out2', pif21, 'in1')
-pif21_h4 = connection(pif21, 'out1', h4.comps['splitter_0'], 'in1', T=ref(k4_pif21, 1, -1*pif21.L.val/100), design=['T'])
+pif21_h4 = connection(pif21, 'out1', h4.comps['splitter_0'], 'in1', T=ref(k4_pif21, 1, -pif21.L.val / dT_feed), design=['T'])
 
 # back
 h4_pib21 = connection(h4.comps['merge_0'], 'out1', pib21, 'in1')
-pib21_k4 = connection(pib21, 'out1', k4.comps['valve_1'], 'in1', T=ref(h4_pib21, 1, -1*pib21.L.val/200), design=['T'])
+pib21_k4 = connection(pib21, 'out1', k4.comps['valve_1'], 'in1', T=ref(h4_pib21, 1, -pib21.L.val / dT_return), design=['T'])
 
 nw.add_conns(k4_pif21, pif21_h4)
 nw.add_conns(h4_pib21, pib21_k4)
