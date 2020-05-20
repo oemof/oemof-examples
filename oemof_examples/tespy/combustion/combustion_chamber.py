a768d1f332477fc6d9dccf420b8ba69873e8416f
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-05-20 16:03:33 +0200

    Update examples to 0.3.X API of TESPy

--- oemof_examples/tespy/combustion/combustion_chamber.py
+++ oemof_examples/tespy/combustion/combustion_chamber.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 19 12:38:43 2019
-
-@author: Malte Fritz
-"""
-
 from tespy.networks import network
 from tespy.components import sink, source, combustion_chamber
 from tespy.connections import connection
@@ -15,8 +9,7 @@ from tespy.connections import connection
 fluid_list = ['Ar', 'N2', 'O2', 'CO2', 'CH4', 'H2O']
 
 # define unit systems and fluid property ranges
-nw = network(fluids=fluid_list, p_unit='bar', T_unit='C',
-             p_range=[0.5, 10], T_range=[10, 1200])
+nw = network(fluids=fluid_list, p_unit='bar', T_unit='C', p_range=[0.5, 10])
 
 # %% components
 
@@ -45,13 +38,20 @@ comb.set_attr(lamb=3, ti=2e6)
 
 # air from abient (ambient pressure and temperature), air composition must be
 # stated component wise.
-amb_comb.set_attr(p=1, T=20, fluid={'Ar': 0.0129, 'N2': 0.7553, 'H2O': 0,
-                                    'CH4': 0, 'CO2': 0.0004, 'O2': 0.2314})
+amb_comb.set_attr(
+    p=1, T=20,
+    fluid={
+        'Ar': 0.0129, 'N2': 0.7553, 'H2O': 0, 'CH4': 0, 'CO2': 0.0004,
+        'O2': 0.2314
+    }
+)
 
 # fuel, pressure must not be stated, as pressure is the same at all inlets and
 # outlets of the combustion chamber
-sf_comb.set_attr(T=25, fluid={'CO2': 0.04, 'Ar': 0, 'N2': 0, 'O2': 0, 'H2O': 0,
-                              'CH4': 0.96})
+sf_comb.set_attr(
+    T=25,
+    fluid={'CO2': 0.04, 'Ar': 0, 'N2': 0, 'O2': 0, 'H2O': 0, 'CH4': 0.96}
+)
 
 # %% solving
 
