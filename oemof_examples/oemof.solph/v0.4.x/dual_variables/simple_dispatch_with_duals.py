20f7ed3d286ec0dd49450d917e26a69d6e59a670
jnnr <32454596+jnnr@users.noreply.github.com>
2022-01-06 15:55:41 +0100

    Drop plot of heat bus

--- oemof_examples/oemof.solph/v0.4.x/dual_variables/simple_dispatch_with_duals.py
+++ oemof_examples/oemof.solph/v0.4.x/dual_variables/simple_dispatch_with_duals.py
@@ -183,26 +183,3 @@ ax1.set_xticklabels(
 )
 
 plt.show()
-
-node_results_bth = views.node(optimization_model.results(), "bth")
-node_results_flows = node_results_bth["sequences"]
-
-fig, ax = plt.subplots(figsize=(10, 5))
-node_results_flows.plot(ax=ax, kind="bar", stacked=True, linewidth=0, width=1)
-ax.set_title("Sums for optimization period")
-ax.legend(loc="upper right", bbox_to_anchor=(1, 1))
-ax.set_xlabel("Energy (MWh)")
-ax.set_ylabel("Flow")
-plt.legend(loc="center left", prop={"size": 8}, bbox_to_anchor=(1, 0.5))
-fig.subplots_adjust(right=0.8)
-
-dates = node_results_flows.index
-tick_distance = int(len(dates) / 7) - 1
-ax.set_xticks(range(0, len(dates), tick_distance), minor=False)
-ax.set_xticklabels(
-    [item.strftime("%d-%m-%Y") for item in dates.tolist()[0::tick_distance]],
-    rotation=90,
-    minor=False,
-)
-
-plt.show()
